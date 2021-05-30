1b518031832eb425ea2ea7b6590ad2dafff41744
Aryanbabar <aryanbabar67@gmail.com>
2021-05-31 00:49:18 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,32 @@
 $(function(){
+  // back-to-top;
+  $('.back-to-top').click(function(){
+    $('html,body').animate({scrollTop:0},1500);
+  });
+  $(window).scroll(function(){
+    var scrolling = $(this).scrollTop();
+    if(scrolling > 300){
+        $('.back-to-top').fadeIn(500);
+    }
+    else {
+        $('.back-to-top').fadeOut(500);
+    }
+  });
+
+  //animation scroll js;
+  var html_body = $('html, body');
+  $('nav a').on('click', function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+              html_body.animate({
+                  scrollTop: target.offset().top - 90
+              }, 1500);
+              return false;
+          }
+      }
+  });
 
   // brand-slider;
   $('.banner-slider').slick({
